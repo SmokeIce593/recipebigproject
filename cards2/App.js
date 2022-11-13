d338376a2cb78be970eff6efcccce45d4bc6b983
Sebastian Perez <perezse2020@gmail.com>
2022-11-12 19:49:14 -0500

    Added Mobile

---
+++ cards2/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import LoginScreen from './screens/LoginScreen';
+import CardScreen from './screens/CardScreen';
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: LoginScreen,
+    navigationOptions: {
+      header: null // Will hide header for HomePage
+    }
+  },
+  Card: {
+    screen: CardScreen,
+    navigationOptions: {
+      header: null // Will hide header for HomePage
+    }
+  }
+},{
+  initialRouteName: "Login"
+});
+const AppContainer = createAppContainer(AppNavigator);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
