f2fd88008b4453fe304c39d306722f22d2ece2c4
krex99 <kadesai39@gmail.com>
2022-11-21 16:51:07 -0500

    redid nutrition api

--- frontend/src/Nutrition.js
+++ frontend/src/Nutrition.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import "Nutrition.css"
+import React from 'react';
+import e from 'express';
 
-const Nutrition = ({ title, ingredients }) => {
-    return(
-        <div className='recipe'>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-        </div>
-    )
+
+const titleInput = document.getElementById('title');
+const recipeInput = document.getElementById('recipe');
+const output = document.getElementById('output');
+const appID = '3c83e59a';
+const appKey ='7562b5bc9d61a7d8ae68edd47888c04a';
+
+function fetchRecipe() {
+    let title = titleInput.value;
+    let ingr = recipeInput.value.split('\n');
+
+    return fetch(`https://api.edamam.com/api/nutrition-details?app_id=${appID}&app_key=${appKey}`, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title, ingr})
+    }).then(response => response.json());
 }
 
-export default Nutrition
+document.getElementById('recipe-check-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    fetchRecipe().then(data => {
+        console.log(data);
+    });
+})
