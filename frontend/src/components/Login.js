f8aa8e7769ea4a15c15d20ae8e9ac4c17a47e380
PolinaSkvortsova <polina.skvort@gmail.com>
2022-11-23 02:50:50 -0500

    commented out camerons test alert box (cringe!!!!)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -44,7 +44,7 @@ function Login()
             else
             {
                 var user = {firstName:res.firstName,lastName:res.lastName,id:res.id,email:res.email, username:res.username, securityquestion:res.securityquestion, securityanswer:res.securityanswer}
-                alert("Email: " + res.email);
+                //alert("Email: " + res.email);
                 localStorage.setItem('user_data', JSON.stringify(user));
 
                 setMessage('');
