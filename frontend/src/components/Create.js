e538c30f10d9cb5c187378dd1cdf5e1ed996ecb9
PolinaSkvortsova <polina.skvort@gmail.com>
2022-11-27 06:46:29 -0500

    nvm it broke for some reason

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -27,6 +27,7 @@ function Create()
         }
     }
 
+	/*
 	// Get the input fields
 	var ingInput = document.getElementById("ingredient");
 	var dirInput = document.getElementById("direction");
@@ -66,6 +67,7 @@ function Create()
 			document.getElementById("addATag").click();
 		}
 	}); 
+	*/
 
 	const createRecipe = async event =>
 	{
@@ -102,26 +104,26 @@ function Create()
 
 	var ingNum = 0;
 	const addIng = async event => {
-		if(document.getElementById('ingredient').value != "")
-		{
-			var table = document.getElementById('ingredientTable');
-			var info = document.getElementById('ingredient').value;
-			ingredient.push(info);
-			let deleteIcon = document.createElement("button");
-				deleteIcon.type = "button";
-				deleteIcon.innerHTML = "X";
-				deleteIcon.onclick = function(){deleteIng(table, this)};
-			var row = table.insertRow(ingNum);
-			var cell1 = row.insertCell(0);
-			var cell2 = row.insertCell(1);
-			var cell3 = row.insertCell(2);
-			cell1.className = "indent"
-			cell3.className = "deleteIcons"
-			cell1.innerHTML = "&bull;";
-			cell2.innerHTML = info;
-			cell3.appendChild(deleteIcon);
-			document.getElementById('ingredient').value = '';
-			ingNum++;
+	if(document.getElementById('ingredient').value != "")
+	{
+		var table = document.getElementById('ingredientTable');
+		var info = document.getElementById('ingredient').value;
+		ingredient.push(info);
+		let deleteIcon = document.createElement("button");
+			deleteIcon.type = "button";
+			deleteIcon.innerHTML = "X";
+			deleteIcon.onclick = function(){deleteIng(table, this)};
+		var row = table.insertRow(ingNum);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		var cell3 = row.insertCell(2);
+		cell1.className = "indent"
+		cell3.className = "deleteIcons"
+		cell1.innerHTML = "&bull;";
+		cell2.innerHTML = info;
+		cell3.appendChild(deleteIcon);
+		document.getElementById('ingredient').value = '';
+		ingNum++;
 		}
 	}
 
@@ -175,24 +177,24 @@ function Create()
 	
 	var dirNum = 0;
 	const addDir = async event => {
-		if(document.getElementById('direction').value != "")
-		{
-			var table = document.getElementById('directionTable');
-			var info = document.getElementById('direction').value;
-			direction.push(info);
-			let deleteIcon = document.createElement("button");
-				deleteIcon.type = "button";
-				deleteIcon.innerHTML = "X";
-				deleteIcon.onclick = function(){deleteDir(table, this)};
-			var row = table.insertRow(dirNum);
-			var cell1 = row.insertCell(0);
-			var cell2 = row.insertCell(1);
-			cell1.className = "info"
-			cell2.className = "deleteIcons"
-			dirNum++;
-			cell1.innerHTML = info;
-			cell2.appendChild(deleteIcon);
-			document.getElementById('direction').value = '';
+	if(document.getElementById('direction').value != "")
+	{
+		var table = document.getElementById('directionTable');
+		var info = document.getElementById('direction').value;
+		direction.push(info);
+		let deleteIcon = document.createElement("button");
+			deleteIcon.type = "button";
+			deleteIcon.innerHTML = "X";
+			deleteIcon.onclick = function(){deleteDir(table, this)};
+		var row = table.insertRow(dirNum);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		cell1.className = "info"
+		cell2.className = "deleteIcons"
+		dirNum++;
+		cell1.innerHTML = info;
+		cell2.appendChild(deleteIcon);
+		document.getElementById('direction').value = '';
 		}
 	}
 	function deleteDir(table, row)
@@ -209,59 +211,59 @@ function Create()
 
    return(
         <div id="createDiv" className="displayregion">
-			<form>
-				<table>
-				<td className = "col1">
-						<input type="text" id="title" placeholder="My New Recipe" className="titlefield" ref={(c) => title = c} /><br />
-						<textarea id="description" className="descfield" placeholder="Recipe Description..." ref={(c) => description = c}/>
-						<div className="lists">
-					</div>
-					<div className="privateBox">Private: 
-						<input
-							id="privaterecipe"
-							type="checkbox"
-							value={checkedRecipe}
-							onChange={handleChange}/>
-					</div>
-				</td>
-
-				<td className = "col2">
-					<div className="lists">
-						<div type='text' className = 'label'>Ingredients:</div>
-						<input type="text" id="ingredient" placeholder="Ingredients..." className="ingField"/><br />
-						<input type="button" id="addIngredient" className="addButton" onClick={addIng} value='+ Add Ingredient' />
-						<table>
-						<tbody id="ingredientTable">
-						</tbody>
-						</table>
-					</div>
-
-					<div className="lists">
-						<div type='text' className = 'label'>Directions:</div>
-						<textarea id="direction" placeholder="Directions..." className="dirField"/><br />
-						<input type="button" id="addDirection" className="addButton" onClick={addDir} value='+ Add Direction' />
-						<table className="numbered">
-						<tbody id="directionTable">
-						</tbody>
-						</table>
-					</div>
-
-					<div className="lists">
-						<div type='text' className = 'label'>Tags:</div>
-						<input type="text" id="tag" placeholder="Tags..." className="tagField"/><br />
-						<input type="button" id="addATag" className="addButton" onClick={addTag} value='+ Add Tag' />
-						<table>
-						<tbody id="tagTable">
-						</tbody>
-						</table>
-					</div>
-				</td>
-				</table>
-				
-				<div id="bumper" className="buffer"><span id="loginResult" className = "error">{message}</span></div> 
-				<input type="submit" id="submitButton" className="submitButton" value ="Save Recipe" onClick={createRecipe} />
-			</form>
+        <form>
+        <table>
+        <td className = "col1">
+            <input type="text" id="title" placeholder="My New Recipe" className="titlefield" ref={(c) => title = c} /><br />
+            <textarea id="description" className="descfield" placeholder="Recipe Description..." ref={(c) => description = c}/>
+            <div className="lists">
+        </div>
+		<div className="privateBox">Private: 
+			<input
+				id="privaterecipe"
+				type="checkbox"
+				value={checkedRecipe}
+				onChange={handleChange}/>
 		</div>
+        </td>
+
+        <td className = "col2">
+        <div className="lists">
+        	<div type='text' className = 'label'>Ingredients:</div>
+        	<input type="text" id="ingredient" placeholder="Ingredients..." className="ingField"/><br />
+        	<input type="button" id="addIngredient" className="addButton" onClick={addIng} value='+ Add Ingredient' />
+        	<table>
+        	<tbody id="ingredientTable">
+        	</tbody>
+        	</table>
+        </div>
+
+        <div className="lists">
+        	<div type='text' className = 'label'>Directions:</div>
+        	<textarea id="direction" placeholder="Directions..." className="dirField"/><br />
+        	<input type="button" id="addDirection" className="addButton" onClick={addDir} value='+ Add Direction' />
+        	<table className="numbered">
+        	<tbody id="directionTable">
+        	</tbody>
+        	</table>
+        </div>
+
+		<div className="lists">
+        	<div type='text' className = 'label'>Tags:</div>
+        	<input type="text" id="tag" placeholder="Tags..." className="tagField"/><br />
+        	<input type="button" id="addATag" className="addButton" onClick={addTag} value='+ Add Tag' />
+        	<table>
+        	<tbody id="tagTable">
+        	</tbody>
+        	</table>
+        </div>
+        </td>
+        </table>
+		
+			<div id="bumper" className="buffer"><span id="loginResult" className = "error">{message}</span></div> 
+            <input type="submit" id="submitButton" className="submitButton" value ="Save Recipe" onClick={createRecipe} />
+        </form>
+        </div>
    );
 };
 
