72d8972cf1e06fddceb602fea68c1be3da4cb9ce
PolinaSkvortsova <polina.skvort@gmail.com>
2022-11-27 06:59:09 -0500

    improved recovery / password reset functionality

--- frontend/src/components/Recover.js
+++ frontend/src/components/Recover.js
@@ -3,63 +3,61 @@ import './recover.css';
 
 const app_name = 'recipeprojectlarge'
 function buildPath(route)
-    {
-        if (process.env.NODE_ENV === 'production') 
-        {
-            return 'https://' + app_name +  '.herokuapp.com/' + route;
-        }
-        else
-        {        
-            return 'http://localhost:5000/' + route;
-        }
-    }
-
-function Recover(){
-
-var email;
-var code;
-var password;
-const [message,setMessage] = useState('');
-
-
-const findEmail = async event => 
-    {
-    	event.preventDefault();
-    	
-    	var obj = {email:email.value};
-        var js = JSON.stringify(obj);
-        
-
-        try
-        {    
-            const response = await fetch(buildPath('api/codecreation'),
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-
-            var res = JSON.parse(await response.text());
-
-            if( res.error !== "")
-            {
-                //setMessage(res.error);
-            }
-            else
-            {
-                //setMessage('');
-                //window.location.href = '/home';
-                document.getElementById("codeDiv").style.visibility = 'visible';
-                document.getElementById("codeDiv").style.display = 'block';
-            }
-        }
-        catch(e)
-        {
-            alert(e.toString());
-            return;
-        }    
-    }
-    
-const verifyCode = async event =>
 {
-    event.preventDefault();
-    	var obj = {code:code.value};
+    if (process.env.NODE_ENV === 'production') 
+    {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else
+    {        
+        return 'http://localhost:5000/' + route;
+    }
+}
+
+function Recover()
+{
+    var email;
+    var code;
+    var password;
+    const [message,setMessage] = useState('');
+
+    const findEmail = async event => 
+        {
+            event.preventDefault();
+            
+            var obj = {email:email.value};
+            var js = JSON.stringify(obj);
+
+            try
+            {    
+                const response = await fetch(buildPath('api/codecreation'),
+                    {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+                var res = JSON.parse(await response.text());
+
+                if( res.error !== "")
+                {
+                    setMessage('Invalid email address');
+                }
+                else
+                {
+                    setMessage('');
+                    //window.location.href = '/home';
+                    document.getElementById("codeDiv").style.visibility = 'visible';
+                    document.getElementById("codeDiv").style.display = 'block';
+                }
+            }
+            catch(e)
+            {
+                alert(e.toString());
+                return;
+            }    
+        }
+        
+    const verifyCode = async event =>
+    {
+        event.preventDefault();
+        var obj = {code:code.value};
         var js = JSON.stringify(obj);
         try
         {    
@@ -75,7 +73,7 @@ const verifyCode = async event =>
             }
             else
             {
-                setMessage('Invalid Code.');
+                setMessage('Invalid code');
                 //window.location.href = '/home';
             }
         }
@@ -83,39 +81,39 @@ const verifyCode = async event =>
         {
             alert(e.toString());
             return;
-        }    
-}
+        }
+    }
 
-const app_name = 'recipeprojectlarge'
+    const app_name = 'recipeprojectlarge'
 
-const goLogin = async event=>
-{
-window.location.href = '/login';
-}
-	
-return(
-	<div>
-	<span id="background-support" className="drop-backgroundimg">⠀</span>
-	<input type="submit" id="returnButton" className="returnButton" value ="Return to Login" onClick={goLogin} />
-	<form onSubmit={findEmail} className="recoverField">
-            <span id="inner-title" className="titlefield">Account Recovery</span><br />
-            <span className="innerText">Enter the e-mail associated with your account:</span><br />
-            <input type="text" id="email" placeholder="Enter your e-mail..." className="inputfield"
-                ref={(c) => email = c} /><br />
-            <div id="bumper" className="buffer"><span id="emailResult" className = "error">{message}</span></div> 
-            <input type="submit" id="submitButton" className="submitButton" value ="Submit" onClick={findEmail} />
-            <div id="codeDiv" className="codeDiv">
-		    <span className="innerText">A verification code has been sent to your e-mail. Please enter the code to verify your account:</span><br />
-		    <div id="bumper" className="buffer"><input type="button" id="recoverLink" className = "recoverLink" value="Didn't get an e-mail? Click here to resend" onClick={findEmail} /></div>
-		    <input type="text" id="code" placeholder="Verification Code" className="inputfield"
-		        ref={(c) => code = c} /><br />
-		    <div id="bumper" className="buffer"><span id="verifyResult" className = "error">{message}</span></div> 
-		    <input type="submit" id="submitButton" className="submitButton" value ="Submit" onClick={verifyCode} />
-            </div>
-        </form>
+    const goLogin = async event=>
+    {
+        window.location.href = '/login';
+    }
+        
+    return(
+        <div>
+        <span id="background-support" className="drop-backgroundimg">⠀</span>
+        <input type="submit" id="returnButton" className="returnButton" value ="Return to Login" onClick={goLogin} />
+        <form onSubmit={findEmail} className="recoverField">
+                <span id="inner-title" className="titlefield">Account Recovery</span><br />
+                <span className="innerText">Enter the e-mail address associated with your account:</span><br />
+                <input type="text" id="email" placeholder="Enter your e-mail..." className="inputfield"
+                    ref={(c) => email = c} /><br />
+                <div id="bumper" className="buffer"><span id="emailResult" className = "error">{message}</span></div> 
+                <input type="submit" id="submitButton" className="submitButton" value ="Send recovery code" onClick={findEmail} />
+                <div id="codeDiv" className="codeDiv">
+                <span className="innerText">A verification code has been sent to your e-mail. Please enter the code to verify your account:</span><br />
+                <div id="bumper" className="buffer"><input type="button" id="recoverLink" className = "recoverLink" value="Didn't get an e-mail? Click here to resend" onClick={findEmail} /></div>
+                <input type="text" id="code" placeholder="Verification Code" className="inputfield"
+                    ref={(c) => code = c} /><br />
+                <div id="bumper" className="buffer"><span id="verifyResult" className = "error">{message}</span></div> 
+                <input type="submit" id="submitButton" className="submitButton" value ="Submit" onClick={verifyCode} />
+                </div>
+            </form>
 
-	</div>
-	);
+        </div>
+    );
 }
 
 export default Recover;
--- frontend/src/components/Reset.js
+++ frontend/src/components/Reset.js
@@ -12,67 +12,89 @@ function Reset()
     const app_name = 'recipeprojectlarge'
 
     function buildPath(route)
+    {
+        if (process.env.NODE_ENV === 'production') 
         {
-            if (process.env.NODE_ENV === 'production') 
-            {
-                return 'https://' + app_name +  '.herokuapp.com/' + route;
-            }
-            else
-            {        
-                return 'http://localhost:5000/' + route;
-            }
+            return 'https://' + app_name +  '.herokuapp.com/' + route;
         }
+        else
+        {        
+            return 'http://localhost:5000/' + route;
+        }
+    }
 
     const resetPassword = async event=>
     {
         event.preventDefault();
-        if(password.value !== passwordconfirm.value){
+        
+        if(password.value != passwordconfirm.value)
+        {
             setMessage("Passwords do not match.");
+            return;
+        }
+        else
+        {
+            setMessage('');
         }
 
-        alert(userId + " " + password.value);
+        var obj = {password:password.value};
+        var js = JSON.stringify(obj);
+
+        var passwordcode = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+        if(!obj.password.match(passwordcode)){
+            setMessage("Your password does not meet requirements: Between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character.")
+            return;
+        }
+        else
+        {
+            setMessage('');
+        }
+
+        //alert(userId + " " + password.value);
         var obj = {id: userId , password:password.value};
         var js = JSON.stringify(obj);
 
         try
-            {    
-                const response = await fetch(buildPath('api/changepassword'),
-                    {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+        {    
+            const response = await fetch(buildPath('api/changepassword'),
+                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
-                var res = JSON.parse(await response.text());
-                alert(res.error);
-                if( res.error !== "")
-                {
-                    setMessage(res.error);
-                }
-                else
-                {
-                    alert("Made to end");
-                    setMessage('');
-                    localStorage.removeItem("user_data")
-                    window.location.href = '/login';   
-                }
-            }
-            catch(e)
+            var res = JSON.parse(await response.text());
+            //alert(res.error);
+            if( res.error !== "")
             {
-                alert(e.toString());
-                return;
-            }    
+                setMessage(res.error);
+            }
+            else
+            {
+                //alert("Made to end");
+                setMessage('');
+                localStorage.removeItem("user_data");
+                window.location.href = '/login'; 
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
     }
 
     const goLogin = async event=>
-        {
-            window.location.href = '/login';
-        }
+    {
+        window.location.href = '/login';
+    }
+
     return(
         <div>
         <span id="background-support" className="drop-backgroundimg">⠀</span>
         <input type="submit" id="returnButton" className="returnButton" value ="Return to Login" onClick={goLogin} />
         <form className="recoverField">
             <span id="inner-title" className="titlefield">Password Reset</span><br />
-            <span className="innerText">Create a new password for your account. Password must be between 8 to 15 characters and contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character:</span><br />
-                <input type="text" id="password1" placeholder="Password" className="inputfield" ref={(c) => password = c} /><br />
-                <input type="text" id="password2" placeholder="Confirm Password" className="inputfield" ref={(c) => passwordconfirm = c} /><br />
+            <span className="innerText">Create a new password for your account. Password must be between 8 to 15 characters and contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character.</span><br />
+                <input type="password" id="password1" placeholder="Password" className="inputfield" ref={(c) => password = c} /><br />
+                <input type="password" id="password2" placeholder="Retype password" className="inputfield" ref={(c) => passwordconfirm = c} /><br />
                 <div id="bumper" className="buffer"><span id="passwordResult" className = "error">{message}</span></div> 
                 <input type="submit" id="submitButton" className="submitButton" value ="Submit" onClick={resetPassword} />
             </form>
